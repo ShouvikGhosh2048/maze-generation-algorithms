8303937540974fa5583f3825c2a324cf19f2ada0
Shouvik Ghosh <91585022+ShouvikGhosh2048@users.noreply.github.com>
2022-08-27 15:40:57 +0530

    Added home page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { Routes, Route } from 'react-router-dom';
+import Home from './Home';
 import SimpleRandom from './SimpleRandom';
 
 function App() {
   return (
     <div>
       <Routes>
+        <Route path="" element={<Home />} />
         <Route path="/visualizations/simple-random" element={<SimpleRandom />} />
       </Routes>
     </div>
---
+++ src/Home.js
@@ -0,0 +1,11 @@
+import { Link } from 'react-router-dom';
+
+function Home() {
+    return (
+        <div>
+            <Link to="/visualizations/simple-random">Simple Random</Link>
+        </div>
+    );
+}
+
+export default Home;
